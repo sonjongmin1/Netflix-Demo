293314e00c6c056cf3b90f5bdfd33b3b59bbd6b5
jong_min <whdals0525@gmail.com>
2024-08-31 19:57:55 +0900

    netflixdemo-08-31

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
-import logo from './logo.svg';
 import './App.css';
+import { Routes, Route } from 'react-router-dom';
+import AppLayout from './layout/AppLayout';
+import Homepage from './pages/Homepage/Homepage';
+import MoviePage from './pages/Movies/MoviePage';
+import MovieDetail from './pages/MovieDetail/MovieDetail';
+import NotFoundPage from './pages/NotFoundPage/NotFoundPage';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+// 홈페이지 /
+// 영화 전체보여주는 페이지 (서치) /movies
+// 영화 디테일 페이지 /movies/:id
+// 추천 영화 /movies/:id/recommandation
+// 리뷰 /movies/:id/reviews
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Routes>
+      <Route path="/" element={<AppLayout />}>
+        <Route index element={<Homepage />} />
+        <Route path="movies">
+          <Route index element={<MoviePage />} />
+          <Route path=":id" element={<MovieDetail />} />
+        </Route>
+      </Route>
+
+      <Route path="*" element={<NotFoundPage />} />
+    </Routes>
   );
 }
 
---
+++ src/layout/AppLayout.jsx
@@ -0,0 +1,56 @@
+import React from 'react';
+import Button from 'react-bootstrap/Button';
+import Container from 'react-bootstrap/Container';
+import Form from 'react-bootstrap/Form';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import { Outlet } from 'react-router-dom';
+import './AppLayout.css';
+
+const AppLayout = () => {
+  return (
+    <div className="nav-background">
+      <Navbar expand="lg">
+        <Container fluid>
+          <Navbar.Brand href="#">
+            <img
+              width={93}
+              height={25.125}
+              src="https://noona-netflix-react-query.vercel.app/netflixLogoSvg.svg"
+              alt="Netflix Logo"
+            />
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="navbarScroll" />
+          <Navbar.Collapse id="navbarScroll">
+            <Nav
+              className="me-auto my-2 my-lg-0"
+              style={{ maxHeight: '100px' }}
+              navbarScroll
+            >
+              <Nav.Link href="#action1" style={{ color: 'white' }}>
+                Home
+              </Nav.Link>
+              <Nav.Link href="#action2" style={{ color: 'white' }}>
+                Movies
+              </Nav.Link>
+            </Nav>
+            <Form className="d-flex">
+              <Form.Control
+                type="search"
+                placeholder="Search"
+                className="me-2"
+                aria-label="Search"
+              />
+              <Button className="custom-red-button" variant="outline-success">
+                Search
+              </Button>
+            </Form>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+      <Outlet />
+    </div>
+  );
+};
+
+export default AppLayout;
