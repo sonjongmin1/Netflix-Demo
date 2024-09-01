784d06e314e2903845fce7e91b6fd0d8bc2b398a
jong_min <whdals0525@gmail.com>
2024-09-02 03:18:59 +0900

    netflixdemo-09-02

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,12 +1,12 @@
-import axios from 'axios';
+import axios from "axios";
 
 const API_KEY = process.env.REACT_APP_API_KEY;
 
 const api = axios.create({
-  baseURL: "'https://api.themoviedb.org/3",
+  baseURL: "https://api.themoviedb.org/3",
   headers: {
-    Accept: 'accept: application/json',
-    Authorization: `Bearer${API_KEY}`,
+    Accept: "application/json",
+    Authorization: `Bearer ${API_KEY}`,
   },
 });
 
