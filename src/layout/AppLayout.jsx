b69c1afcf80f8ee0063fbac7da78356dd9fe83c9
jong_min <whdals0525@gmail.com>
2024-09-04 21:27:12 +0900

    netflixdemo-09-04-c

--- src/layout/AppLayout.jsx
+++ src/layout/AppLayout.jsx
@@ -1,18 +1,28 @@
-import React from 'react';
-import Button from 'react-bootstrap/Button';
-import Container from 'react-bootstrap/Container';
-import Form from 'react-bootstrap/Form';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
-import { Outlet } from 'react-router-dom';
-import './AppLayout.css';
+import React, { useState } from "react";
+import Button from "react-bootstrap/Button";
+import Container from "react-bootstrap/Container";
+import Form from "react-bootstrap/Form";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import { Outlet, useNavigate } from "react-router-dom";
+import "./AppLayout.css";
 
 const AppLayout = () => {
+  const [keyword, setKeyword] = useState("");
+  const navigate = useNavigate();
+
+  const searchByKeyWord = (event) => {
+    event.preventDefault();
+    //url을 바꿔주기
+    navigate(`/movies?q=${keyword}`);
+    setKeyword("");
+  };
+
   return (
     <div className="nav-background">
       <Navbar expand="lg">
         <Container fluid>
-          <Navbar.Brand href="#">
+          <Navbar.Brand href="/">
             <img
               width={93}
               height={25.125}
@@ -24,24 +34,30 @@ const AppLayout = () => {
           <Navbar.Collapse id="navbarScroll">
             <Nav
               className="me-auto my-2 my-lg-0"
-              style={{ maxHeight: '100px' }}
+              style={{ maxHeight: "100px" }}
               navbarScroll
             >
-              <Nav.Link href="#action1" style={{ color: 'white' }}>
-                Home
+              <Nav.Link href="/" style={{ color: "white" }}>
+                홈
               </Nav.Link>
-              <Nav.Link href="#action2" style={{ color: 'white' }}>
-                Movies
+              <Nav.Link href="/movies" style={{ color: "white" }}>
+                영화
               </Nav.Link>
             </Nav>
-            <Form className="d-flex">
+            <Form className="d-flex" onSubmit={searchByKeyWord}>
               <Form.Control
                 type="search"
                 placeholder="Search"
                 className="me-2"
                 aria-label="Search"
+                value={keyword}
+                onChange={(event) => setKeyword(event.target.value)}
               />
-              <Button className="custom-red-button" variant="outline-success">
+              <Button
+                className="custom-red-button"
+                variant="outline-success"
+                type="submit"
+              >
                 Search
               </Button>
             </Form>
--- src/pages/Movies/MoviePage.jsx
+++ src/pages/Movies/MoviePage.jsx
@@ -1,7 +1,88 @@
-import React from 'react';
+import React, { useState } from "react";
+import { useSearchMovieQuery } from "../../hooks/useSearchMovie";
+import { useSearchParams } from "react-router-dom";
+import { Container, Spinner, Row, Col, Alert } from "react-bootstrap";
+import MovieCard from "../../common/MovieCard/MovieCard";
+import ReactPaginate from "react-paginate";
+import "./MoviePage.style.css";
+
+//경로 2가지
+//nav바에서 클릭해서 온경우 => popularMovie 보여주기
+//keyword를 입력해서 온경우 => keyword와 관련된 영화들을 보여줌
+
+//페이지네이션 설치
+//page state 만들기
+//페이지네이션 클릭할때마다 page 바꿔주기
+//page 값이 바뀔때 마다 useSearchMovie에 page까지 넣어서 fetch
 
 const MoviePage = () => {
-  return <div></div>;
+  const [query, setQuery] = useSearchParams();
+  const [page, setPage] = useState(1);
+  const keyword = query.get("q");
+
+  const { data, isLoading, isError, error } = useSearchMovieQuery({
+    keyword,
+    page,
+  });
+
+  const handlePageClick = ({ selected }) => {
+    setPage(selected + 1);
+  };
+  console.log("ddd", data);
+
+  if (isLoading) {
+    return (
+      <div className="spinner-area">
+        <Spinner
+          animation="border"
+          variant="danger"
+          style={{ width: "5rem", height: "5rem" }}
+        />
+      </div>
+    );
+  }
+
+  if (isError) {
+    return <Alert variant="danger"> {error.message}</Alert>;
+  }
+
+  return (
+    <Container>
+      <Row>
+        <Col lg={4} xs={12}></Col>
+        <Col lg={8} xs={12}>
+          <Row>
+            {data?.results.map((movie, index) => (
+              <Col key={index} lg={4} xs={12}>
+                <MovieCard movie={movie} />
+              </Col>
+            ))}
+          </Row>
+          <ReactPaginate
+            nextLabel="다음 >"
+            onPageChange={handlePageClick}
+            pageRangeDisplayed={3}
+            marginPagesDisplayed={2}
+            pageCount={data?.total_pages}
+            previousLabel="< 이전"
+            pageClassName="page-item"
+            pageLinkClassName="page-link"
+            previousClassName="page-item"
+            previousLinkClassName="page-link"
+            nextClassName="page-item"
+            nextLinkClassName="page-link"
+            breakLabel="..."
+            breakClassName="page-item"
+            breakLinkClassName="page-link"
+            containerClassName="pagination"
+            activeClassName="active"
+            renderOnZeroPageCount={null}
+            forcePage={page - 1}
+          />
+        </Col>
+      </Row>
+    </Container>
+  );
 };
 
 export default MoviePage;
